35a37b09079a7eec89fbec6813badce78630705a
Nacho Casado <nachoelcalvo@gmail.com>
2017-10-20 13:42:18 +0200

    Refactoring

--- src/main/java/com/jcasado/poc/Application.java
+++ src/main/java/com/jcasado/poc/Application.java
@@ -1,4 +1,4 @@
-package me.nicolaferraro.quickstarts;
+package com.jcasado.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/jcasado/poc/SampleController.java
+++ src/main/java/com/jcasado/poc/SampleController.java
@@ -1,4 +1,4 @@
-package me.nicolaferraro.quickstarts;
+package com.jcasado.poc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
